e478ae26a798f23d95d055324ed6b690157ccff5
Olha Cheroi <107466851+olya4e@users.noreply.github.com>
2022-09-08 13:41:38 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+let intervalId = null;
 
+const onStartBtnClick = e => {
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnEl.setAttribute('disabled', true);
+};
+
+const onStopBtnClick = e => {
+  clearInterval(intervalId);
+  startBtnEl.removeAttribute('disabled');
+};
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
